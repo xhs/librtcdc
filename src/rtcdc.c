cd19d6402657ac6f763017ae5633e15aebdfdb9a
xhs <chef@dark.kitchen>
2015-02-01 20:14:58 +0800

    add empty file

---
+++ src/rtcdc.c
@@ -0,0 +1,7 @@
+// rtcdc.c
+// Copyright (c) 2015 Xiaohan Song <chef@dark.kitchen>
+// This file is licensed under a GNU GPLv3 license.
+
+#include "rtcdc.h"
+
+
---
+++ src/rtcdc.h
@@ -0,0 +1,18 @@
+// rtcdc.h
+// Copyright (c) 2015 Xiaohan Song <chef@dark.kitchen>
+// This file is licensed under a GNU GPLv3 license.
+
+#ifndef _RTCDC_H_
+#define _RTCDC_H_
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif // _RTCDC_H_
