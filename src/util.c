791568a62b82bc993634735a130f87a8bb0799e4
xhs <chef@dark.kitchen>
2015-02-05 19:04:37 +0800

    fix random_number_string()

--- src/util.c
+++ src/util.c
@@ -21,8 +21,9 @@ void
 random_number_string(char *dest, int len)
 {
   const static char *numbers = "0123456789";
+  srand(time(NULL));
   for (int i = 0; i < len; ++i) {
-    int r = random_integer(0, 9);
+    int r = rand() % 10;
     *(dest + i) = numbers[r];
   }
 }
