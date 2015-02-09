2c4f8062bbe9c5dd865a618001221859a76b0aab
xhs <chef@dark.kitchen>
2015-02-09 18:25:53 +0800

    add DCEP definition

---
+++ src/dcep.h
@@ -0,0 +1,47 @@
+// dcep.h
+// Copyright (c) 2015 Xiaohan Song <chef@dark.kitchen>
+// This file is licensed under a GNU GPLv3 license.
+
+#ifndef _RTCDC_DCEP_H_
+#define _RTCDC_DCEP_H_
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+#define DATA_CHANNEL_OPEN 0x03
+#define DATA_CHANNEL_ACK  0x02
+
+#define DATA_CHANNEL_RELIABLE                          0x00
+#define DATA_CHANNEL_RELIABLE_UNORDERED                0x80
+#define DATA_CHANNEL_PARTIAL_RELIABLE_REXMIT           0x01
+#define DATA_CHANNEL_PARTIAL_RELIABLE_REXMIT_UNORDERED 0x81
+#define DATA_CHANNEL_PARTIAL_RELIABLE_TIMED            0x02
+#define DATA_CHANNEL_PARTIAL_RELIABLE_TIMED_UNORDERED  0x82
+
+#define DATA_CHANNEL_PRIORITY_BELOW_NORMAL 128
+#define DATA_CHANNEL_PRIORITY_NORMAL       256
+#define DATA_CHANNEL_PRIORITY_HIGH         512
+#define DATA_CHANNEL_PRIORITY_EXTRA_HIGH   1024
+
+struct dcep_open_message __attribute__((packed, aligned(1))) {
+  uint8_t message_type;
+  uint8_t channel_type;
+  uint16_t priority;
+  uint32_t reliability_param;
+  uint16_t label_length;
+  uint16_t protocol_length;
+  char label_and_protocol[0];
+};
+
+struct dcep_ack_message __attribute__((packed, aligned(1))) {
+  uint8_t message_type;
+};
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif // _RTCDC_DCEP_H_
