614eccf2ea1c23fbf9ea7487cafb01f4528c816a
xhs <chef@dark.kitchen>
2015-02-20 22:50:26 +0800

    define librtcdc interface

--- src/rtcdc.h
+++ src/rtcdc.h
@@ -9,7 +9,59 @@
 extern "C" {
 #endif
 
+#ifndef RTCDC_MAX_CHANNEL_NUM
+#define RTCDC_MAX_CHANNEL_NUM 128
+#endif
 
+struct rtcdc_transport;
+
+struct rtcdc_data_channel {
+  uint8_t type;
+  uint16_t priority;
+  uint32_t rtx;
+  uint32_t lifetime;
+  char *label;
+  char *protocol;
+  int state;
+  uint16_t sid;
+  void (*on_message)(struct rtcdc_data_channel *channel,
+                     int datatype, void *data, size_t len);
+};
+
+struct rtcdc_peer_connection {
+  struct rtcdc_transport *transport;
+  struct rtcdc_data_channel *channels[RTCDC_MAX_CHANNEL_NUM];
+  void (*on_channel)(struct rtcdc_data_channel *channel);
+};
+
+struct rtcdc_peer_connection *
+rtcdc_create_peer_connection(void (*on_channel)(struct rtcdc_data_channel *channel));
+
+void
+rtcdc_destroy_peer_connection(struct rtcdc_peer_connection *peer);
+
+char *
+rtcdc_create_offer_sdp(struct rtcdc_peer_connection *peer);
+
+int
+rtcdc_create_answer_sdp(struct rtcdc_peer_connection *peer, const char *offer);
+
+char **
+rtcdc_generate_local_candidates(struct rtcdc_peer_connection *peer, int *num);
+
+int
+rtcdc_parse_remote_candidate(struct rtcdc_peer_connection *peer, const char *candidate);
+
+struct rtcdc_data_channel *
+rtcdc_create_reliable_data_channel(const char *label, const char *protocol,
+                                   void (*on_message)(struct rtcdc_data_channel *channel,
+                                                      int datatype, void *data, size_t len));
+
+void
+rtcdc_destroy_data_channel(struct rtcdc_data_channel *channel);
+
+int
+rtcdc_send_message(struct rtcdc_data_channel *channel, int datatype, void *data, size_t len);
 
 #ifdef  __cplusplus
 }
