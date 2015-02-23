69623478523b85875e1b12f64d49d8fe9ce46570
xhs <chef@dark.kitchen>
2015-02-23 18:21:04 +0800

    add example.c; remove test.c

---
+++ src/example.c
@@ -0,0 +1,13 @@
+// example.c
+// Copyright (c) 2015 Xiaohan Song <chef@dark.kitchen>
+// This file is licensed under a GNU GPLv3 license.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "rtcdc.h"
+
+int main(int argc, char *argv[])
+{
+  return 0;
+}
